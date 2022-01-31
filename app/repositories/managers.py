a5f34e6bf8da4f31adbe53ce85987973be76cca2
Wladymir Brborich <wabrborich@hotmail.com>
2022-01-31 18:51:05 -0500

    Implement all managers basic database interaction methods
    * Implement get_all, get_by_id, create, and update as generic manager methods
    
    * Override the OrderManager create method to include custom logic
    
    * Update to ma.SQLAlchemyAutoSchema since ModelSchema was deprecated
    
    * Include a static reference to the database session as a class attribute in the base manager (this allows all child's to access the same configured session)
    
    * override OrderManager.update() since we don't support that method

--- app/repositories/managers.py
+++ app/repositories/managers.py
@@ -1,13 +1,14 @@
-from typing import Optional
+from typing import Any, Optional, Sequence, List
 
-from .models import Ingredient, Order, Size, db
+from .models import Ingredient, Order, OrderDetail, Size, db
 from .serializers import (IngredientSerializer, OrderSerializer,
                           SizeSerializer, ma)
 
 
 class BaseManager:
     model: Optional[db.Model] = None
-    serializer: Optional[ma.ModelSchema] = None
+    serializer: Optional[ma.SQLAlchemyAutoSchema] = None
+    db = db
 
     @classmethod
     def get_all(cls):
@@ -15,6 +16,26 @@ class BaseManager:
         _objects = cls.model.query.all()
         result = serializer.dump(_objects)
         return result
+
+    @classmethod
+    def get_by_id(cls, _id: Any):
+        entry = cls.model.query.get(_id)
+        return cls.serializer().dump(entry)
+
+    @classmethod
+    def create(cls, entry: dict):
+        serializer = cls.serializer()
+        new_entry = serializer.load(entry)
+        cls.db.session.add(new_entry)
+        cls.db.session.commit()
+        return serializer.dump(new_entry)
+
+    @classmethod
+    def update(cls, _id: Any, new_values: dict):
+        entry = cls.get_by_id(_id)
+        entry.update(new_values)
+        cls.db.session.commit()
+        return cls.serializer().dump(entry)
 
 
 class SizeManager(BaseManager):
@@ -26,11 +47,26 @@ class IngredientManager(BaseManager):
     model = Ingredient
     serializer = IngredientSerializer
 
+    @classmethod
+    def get_by_id_list(cls, ids: Sequence):
+        return cls.model.filter(cls.model._id.in_(set(ids))).all()
+
 
 class OrderManager(BaseManager):
     model = Order
     serializer = OrderSerializer
 
     @classmethod
-    def create_order(cls, db, order):
-        pass
+    def create(cls, order_data: dict, ingredients: List[Ingredient]):
+        new_order = cls.model(**order_data)
+        cls.db.session.add(new_order)
+        cls.db.session.flush()
+        cls.db.session.refresh(new_order)
+        cls.db.session.add_all((OrderDetail(order_id=new_order._id, ingredient_id=ingredient._id, ingredient_price=ingredient.price)
+                                for ingredient in ingredients))
+        cls.db.session.commit()
+        return cls.serializer().dump(new_order)
+
+    @classmethod
+    def update(cls):
+        raise NotImplementedError(f'Method not suported for {cls.__name__}')
