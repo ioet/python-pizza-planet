54403b0bb4d446ad961953e93ad209b1636991a3
Wladymir Brborich <wabrborich@hotmail.com>
2022-01-31 19:00:27 -0500

    Implement ingredient service
    * this implementation of the ingredient service calls the basic controllers and implements all of its functionality

--- app/services/ingredient.py
+++ app/services/ingredient.py
@@ -1,24 +1,38 @@
 from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+
+from ..controllers import IngredientController
 
 ingredient = Blueprint('ingredient', __name__)
 
 
 @ingredient.route('/', methods=POST)
 def create_ingredient():
-    pass
+    ingredient, error = IngredientController.create(request.json)
+    response = ingredient if not error else {'error': error}
+    status_code = 200 if not error else 400
+    return jsonify(response), status_code
 
 
 @ingredient.route('/', methods=PUT)
 def update_ingredient():
-    pass
+    ingredient, error = IngredientController.update(request.json)
+    response = ingredient if not error else {'error': error}
+    status_code = 200 if not error else 400
+    return jsonify(response), status_code
 
 
 @ingredient.route('/id/<_id>', methods=GET)
 def get_ingredient_by_id(_id: int):
-    pass
+    ingredient, error = IngredientController.get_by_id(_id)
+    response = ingredient if not error else {'error': error}
+    status_code = 200 if ingredient else 404 if not error else 400
+    return jsonify(response), status_code
 
 
 @ingredient.route('/', methods=GET)
 def get_ingredients():
-    pass
+    ingredients, error = IngredientController.get_all()
+    response = ingredients if not error else {'error': error}
+    status_code = 200 if ingredients else 404 if not error else 400
+    return jsonify(response), status_code
