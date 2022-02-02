11dbbb76791aa18bd4d9fd469a38bd6f5cc6b691
Wladymir Brborich <wabrborich@hotmail.com>
2022-02-01 20:35:02 -0500

    Implement size service functionality
    * Implement the base API from the manager to create, update and fetch sizes

--- app/services/size.py
+++ app/services/size.py
@@ -1,19 +1,30 @@
 from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+
+from ..controllers import SizeController
 
 size = Blueprint('size', __name__)
 
 
 @size.route('/', methods=POST)
 def create_size():
-    pass
+    size, error = SizeController.create(request.json)
+    response = size if not error else {'error': error}
+    status_code = 200 if not error else 400
+    return jsonify(response), status_code
 
 
 @size.route('/', methods=PUT)
 def update_size():
-    pass
+    size, error = SizeController.update(request.json)
+    response = size if not error else {'error': error}
+    status_code = 200 if not error else 400
+    return jsonify(response), status_code
 
 
 @size.route('/id/<_id>', methods=GET)
 def get_size_by_id(_id: int):
-    pass
+    size, error = SizeController.get_by_id(_id)
+    response = size if not error else {'error': error}
+    status_code = 200 if size else 404 if not error else 400
+    return jsonify(response), status_code
