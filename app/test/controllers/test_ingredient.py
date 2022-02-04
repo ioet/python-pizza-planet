ec9af859c66f6c45e35fdedee07ba538f2c37d86
Wladymir Brborich <wabrborich@hotmail.com>
2022-02-03 22:23:38 -0500

    Add tests for Ingredient controller

---
+++ app/test/controllers/test_ingredient.py
@@ -0,0 +1,50 @@
+import pytest
+from app.controllers import IngredientController
+
+
+def test_create(ingredient: dict):
+    created_ingredient, error = IngredientController.create(ingredient)
+    pytest.assume(error is None)
+    for param, value in ingredient.items():
+        pytest.assume(param in created_ingredient)
+        pytest.assume(value == created_ingredient[param])
+        pytest.assume(created_ingredient['_id'])
+
+
+def test_update(ingredient: dict):
+    created_ingredient, _ = IngredientController.create(ingredient)
+    updated_fields = {
+        'name': 'updated',
+        'price': 10
+    }
+    updated_ingredient, error = IngredientController.update({
+        '_id': created_ingredient['_id'],
+        **updated_fields
+    })
+    pytest.assume(error is None)
+    for param, value in updated_fields.items():
+        pytest.assume(updated_ingredient[param] == value)
+
+
+def test_get_by_id(ingredient: dict):
+    created_ingredient, _ = IngredientController.create(ingredient)
+    ingredient_from_db, error = IngredientController.get_by_id(created_ingredient['_id'])
+    pytest.assume(error is None)
+    for param, value in created_ingredient.items():
+        pytest.assume(ingredient_from_db[param] == value)
+
+
+def test_get_all(ingredient: list):
+    created_ingredients = []
+    for _ in range(5):
+        created_ingredient, _ = IngredientController.create(ingredient)
+        created_ingredients.append(created_ingredient)
+
+    ingredients_from_db, error = IngredientController.get_all()
+    searchable_ingredients = {db_ingredient['_id']: db_ingredient for db_ingredient in ingredients_from_db}
+    pytest.assume(error is None)
+    for created_ingredient in created_ingredients:
+        current_id = created_ingredient['_id']
+        assert current_id in searchable_ingredients
+        for param, value in created_ingredient.items():
+            pytest.assume(searchable_ingredients[current_id][param] == value)
