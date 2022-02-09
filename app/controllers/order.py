d675d7309e343f280a34438d35153fdbdb07ff3a
Wladymir Brborich <wabrborich@hotmail.com>
2022-02-09 11:09:01 -0800

    Implement custom create method for Order (#6)
    * Add python specific .gitignore
    
    * Create initial flask app configuration and initialization
    
    * Create config class to manage project configurations
    
    *  Add support for dynamic blueprint loading
    
    * Add support for app CORS
    
    * Add support for serializers, and database models
    
    * Add some syntax aliases for http methods
    
    * Flask uses lists by default, Its better to use a constant to avoid always rewriting the same list
    
    * Add support for plugins
    
    * Create a file to initialize app plugins
    
    * Initialize marshmallow and sqlalchemy to add database and json serialization support.
    
    * Create wireframe for size service
    
    * Add a blueprint for size
    
    * draft a placeholder for all the necesary http methods for size
    
    * Create wireframe for order service
    
    * Add a blueprint for order
    
    * draft a placeholder for all the necesary http methods for order
    
    * Create wireframe for ingredient service
    
    * Add a blueprint for ingredient
    
    * draft a placeholder for all the necesary http methods for ingredient
    
    * Add a general module file to manage service imports
    
    * This allows for dynamic blueprint loading at initialization
    
    * Create database models
    
    * Create models for Ingredient, Size, OrderDetail, and Order
    
    * This models will be used to migrate the database
    
    * Create serializers for database models
    
    * Serializers conform to the database structure, and add support for nested models and backrefs
    
    * Create managers to contain database operations
    
    * Add a wireframe for the base manager
    
    * Add placeholders for all other necesary manager implementations (ingredient, size, and order)
    
    * Create a management script to run and test the application
    
    * This script allows you to run the application, migrate the database and run tests.
    
    * Test are not implemented yet
    
    * Add initial round of requirements for the project
    
    * Implement all managers basic database interaction methods
    
    * Implement get_all, get_by_id, create, and update as generic manager methods
    
    * Override the OrderManager create method to include custom logic
    
    * Update to ma.SQLAlchemyAutoSchema since ModelSchema was deprecated
    
    * Include a static reference to the database session as a class attribute in the base manager (this allows all child's to access the same configured session)
    
    * override OrderManager.update() since we don't support that method
    
    * Update serializers to use the new Marshmallow API
    
    * use SQLAlchemyAutoSchema instead of ModelSchema
    
    * add load_instance=True so marshmallow can parse effectively the json values to a model
    
    * Implement a the base controller
    
    * the basic controller contains a basic API that calls directly the manager with basic error handling
    
    * We can update the specific logic in each particular controller
    
    * Implement basic controllers for ingredient, order, and size
    
    * this basic controllers just inherit all the functionality from the base controller
    
    * We need to update the custom business logic, mainly in the order controller
    
    * Tidy controller import
    
    * Implement ingredient service
    
    * this implementation of the ingredient service calls the basic controllers and implements all of its functionality
    
    * Implement size service functionality
    
    * Implement the base API from the manager to create, update and fetch sizes
    
    * Create the utils file to place all generic logic
    
    * Create check_required_keys which validates a dict for a set of valid keys
    
    * Fix IngredientManager.get_by_id_list()
    
    * Implement the correct sqlalchemy api to fetch instances filtering by a list of ids
    
    * Implement custom create for Orders
    
    * Implement all the business logic that calculates the order price
    
    * Implement all the custom validations needed to create an order

--- app/controllers/order.py
+++ app/controllers/order.py
@@ -1,6 +1,40 @@
-from ..repositories.managers import OrderManager
 from .base import BaseController
+from ..repositories.managers import OrderManager
+from ..repositories.managers import (IngredientManager, OrderManager,
+                                     SizeManager)
+from ..common.utils import check_required_keys
+from sqlalchemy.exc import SQLAlchemyError
+<< << << < HEAD
+
+== == == =
+>>>>>> > main
 
 
 class OrderController(BaseController):
     manager = OrderManager
+    __required_info = ('client_name', 'client_dni', 'client_address', 'client_phone', 'size_id')
+
+    @staticmethod
+    def calculate_order_price(size_price: float, ingredients: list):
+        price = size_price + sum(ingredient.price for ingredient in ingredients)
+        return round(price, 2)
+
+    @classmethod
+    def create(cls, order: dict):
+        if not check_required_keys(cls.__required_info, order):
+            return 'Invalid order payload', None
+
+        size_id = order.get('size_id')
+        size = SizeManager.get_by_id(size_id)
+
+        if not size:
+            return 'Invalid size for Order', None
+
+        ingredient_ids = order.pop('ingredients', [])
+        try:
+            ingredients = IngredientManager.get_by_id_list(ingredient_ids)
+            price = cls.calculate_order_price(size.get('price'), ingredients)
+            order_with_price = {**order, 'total_price': price}
+            return cls.manager.create(order_with_price, ingredients), None
+        except (SQLAlchemyError, RuntimeError) as ex:
+            return None, str(ex)
