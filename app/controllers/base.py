b346c097887f7df68fe4a68c8ce3af1abfbdb51a
Wladymir Brborich <wabrborich@hotmail.com>
2022-04-12 01:03:26 -0500

    Improve base type hints to clarify the return types
    It might be confusing to have a tuple returned when you expect just one object

--- app/controllers/base.py
+++ app/controllers/base.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Any, Optional, Tuple
 from sqlalchemy.exc import SQLAlchemyError
 from ..repositories.managers import BaseManager
 
@@ -7,28 +7,28 @@ class BaseController:
     manager: Optional[BaseManager] = None
 
     @classmethod
-    def get_by_id(cls, _id: Any):
+    def get_by_id(cls, _id: Any) -> Tuple[Any, Optional[str]]:
         try:
             return cls.manager.get_by_id(_id), None
         except (SQLAlchemyError, RuntimeError) as ex:
             return None, str(ex)
 
     @classmethod
-    def get_all(cls):
+    def get_all(cls) -> Tuple[Any, Optional[str]]:
         try:
             return cls.manager.get_all(), None
         except (SQLAlchemyError, RuntimeError) as ex:
             return None, str(ex)
 
     @classmethod
-    def create(cls, entry: dict):
+    def create(cls, entry: dict) -> Tuple[Any, Optional[str]]:
         try:
             return cls.manager.create(entry), None
         except (SQLAlchemyError, RuntimeError) as ex:
             return None, str(ex)
 
     @classmethod
-    def update(cls, new_values: dict):
+    def update(cls, new_values: dict) -> Tuple[Any, Optional[str]]:
         try:
             _id = new_values.pop('_id', None)
             if not _id:
