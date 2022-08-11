44957faa7edb47d4b410bcc16b76e34efae77717
KryzAle <kryzale@gmail.com>
2022-08-11 17:03:44 -0500

    fix: Fix order price calculation
    The order price now is included in the sum of the ingredients plus the pizza size

--- app/controllers/order.py
+++ app/controllers/order.py
@@ -8,11 +8,12 @@ from .base import BaseController
 
 class OrderController(BaseController):
     manager = OrderManager
-    __required_info = ('client_name', 'client_dni', 'client_address', 'client_phone', 'size_id')
+    __required_info = ('client_name', 'client_dni','client_address', 'client_phone', 'size_id')
 
     @staticmethod
     def calculate_order_price(size_price: float, ingredients: list):
-        price = sum(ingredient.price for ingredient in ingredients)
+        ingredients_price = sum(ingredient.price for ingredient in ingredients)
+        price = ingredients_price + size_price
         return round(price, 2)
 
     @classmethod
