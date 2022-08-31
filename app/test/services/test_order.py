76138991edebb18d6fcf07b811611b10228d0a90
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-08-31 13:10:10 -0500

    adding get_orders service test

--- app/test/services/test_order.py
+++ app/test/services/test_order.py
@@ -2,15 +2,22 @@ import pytest
 
 
 def test_create_order_service(create_orders):
-    for created_order in create_orders:
-        order = created_order.json
-        pytest.assume(created_order.status.startswith('200'))
-        pytest.assume(order['_id'])
-        pytest.assume(order['client_address'])
-        pytest.assume(order['client_dni'])
-        pytest.assume(order['client_name'])
-        pytest.assume(order['client_phone'])
-        pytest.assume(order['date'])
-        pytest.assume(order['detail'])
-        pytest.assume(order['size'])
-        pytest.assume(order['total_price'])
+    created_order = create_orders[0]
+    order = created_order.json
+    pytest.assume(created_order.status.startswith('200'))
+    pytest.assume(order['_id'])
+    pytest.assume(order['client_address'])
+    pytest.assume(order['client_dni'])
+    pytest.assume(order['client_name'])
+    pytest.assume(order['client_phone'])
+    pytest.assume(order['date'])
+    pytest.assume(order['detail'])
+    pytest.assume(order['size'])
+    pytest.assume(order['total_price'])
+
+def test_get_orders_service(client, create_orders, order_uri):
+    response = client.get(order_uri)
+    pytest.assume(response.status.startswith('200'))
+    returned_orders = {order['_id']: order for order in response.json}
+    for order in create_orders:
+        pytest.assume(order.json['_id'] in returned_orders)
