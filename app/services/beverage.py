59342a2107f2a338edc986c4a1737cf0edb0e616
jorgesalinas <jorgesalinas7140@gmail.com>
2022-09-02 10:14:26 -0500

    feat: Add interface <BaseService> for the size, ingredient, beverage an order following builder creational design pattern

--- app/services/beverage.py
+++ app/services/beverage.py
@@ -1,38 +1,28 @@
 from app.common.http_methods import POST, PUT, GET
-from flask import Blueprint, request, jsonify
-
+from flask import Blueprint, request
 from app.controllers.beverage import BeverageController
+from app.services.base import BaseService
 
 
 beverage = Blueprint('beverage', __name__)
 
+beverage_base_service = BaseService(entity = "ingredient", 
+        entitycontroller = BeverageController())
+
 @beverage.route('/', methods=POST)
 def create_beverage():
-    beverage, error = BeverageController.create(request.json)
-    response = beverage if not error else {'error':error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return beverage_base_service.create(request)
 
 
 @beverage.route('/', methods=PUT)
 def update_beverage():
-    beverage, error = BeverageController.update(request.json)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
-
+    return beverage_base_service.update(request)
 
 @beverage.route('/id/<_id>', methods=GET)
 def get_beverage_by_id(_id: int):
-    beverage, error = BeverageController.get_by_id(_id)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if beverage else 404 if not error else 400
-    return jsonify(response), status_code
+    return beverage_base_service.get_by_id(_id)
 
 @beverage.route('/', methods=GET)
 def get_beverage():
-    beverages, error = BeverageController.get_all()
-    response = beverages if not error else {'error': error}
-    status_code = 200 if beverages else 404 if not error else 400
-    return jsonify(response), status_code 
+    return beverage_base_service.get_all() 
     
