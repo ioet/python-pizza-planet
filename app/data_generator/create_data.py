23b6696abf1c36a98b9d9ad34fd62bae014ebcf3
Ivan Corniel <ivancorniel30@gmail.com>
2022-09-05 06:49:07 -0400

    implementing the scripts to add orders

--- app/data_generator/create_data.py
+++ app/data_generator/create_data.py
@@ -1,13 +1,14 @@
-from app.data_generator.db_connector import insert_data, insert_order, insert_order_detail
-from app.data_generator.db_connector import get_ingredient, get_size
-from tqdm import tqdm
-from app.data_generator.data_generator import CustomerGenerator
-from app.test.utils.functions import *
 from datetime import datetime
 from random import randint
 from tqdm import tqdm
+from app.controllers import ingredient
+from app.data_generator.data_generator import CustomerGenerator
+from app.controllers.order import OrderController
+from app.repositories.managers import OrderManager
+from app.services.order import create_order
 
 def main():
+
 
     sizes = [('Mini', '5.0'), ('Small', '7.0'), ('Medium', '12.0'), ('XBIG', '17')]
     Ingredients = [('Mushrooms', '5.0'), ('Veggie ', '7.0'), ('Double cheese', '7.0'), ('Triple cheese', '10.0'), ('Special', '10.0'), ('Super special', '15.0'), ('Ham', '5.0')]
@@ -40,32 +41,20 @@ def main():
         'Adams Marie', 
     ]
 
-    customers = []
+    order = {
+        "client_address": "c 12 #36",
+        "client_dni": "1234",
+        "client_name": "Ivan",
+        "client_phone": "555-555-5555",
+        "ingredients": [
+            "7"
+        ],
+        "size_id": "17"
+    }
 
-    for name in names:
-        customers.append(
-            CustomerGenerator.create_dummy_data(
-            customer_name = name,
-            customer_dni = get_random_sequence(),
-            customer_address = get_random_string(),
-            customer_phone = get_random_sequence()
-            )
-        )
-        
+    print(OrderController.create(order))
 
-    def create_orders():
-        
-        for round in tqdm(range(1)):
-            order_owner = customers[randint(1,15)]
-            ingredient = get_ingredient(randint(1,10))
-            size = get_size(randint(13,17))
 
-            insert_order(
-                '"order"', 
-                'client_name,  client_dni, client_address, client_phone, date, total_price, size_id', 
-                ['John', '1234', 'c7# 22', '555-555-5556', '2022-02-22 00:00:00.0', '15.0', '14']
-            )
-    create_orders()
 
 if __name__ == '__main__':
     main()
--- app/test/data_generator/test_data_generator.py
+++ app/test/data_generator/test_data_generator.py
@@ -1,49 +1,7 @@
-# import pytest
-# from app.data_generator.data_generator import CustomerGenerator, OrderGenerator, IngredientGenerator, SizeGenerator
+import pytest
+from app.data_generator import create_data 
 
 
-# from ..utils.functions import *
+def test_dummy_orders_created():
 
-
-# def test_customer_generator():
-    
-#     assert CustomerGenerator.create_dummy_data(
-#             customer_address = get_random_string(),
-#             customer_dni = get_random_sequence(),
-#             customer_name = get_random_string(),
-#             customer_phone = get_random_sequence()
-#     )
-
-# def test_ingredient_generator():
-
-#     assert IngredientGenerator.create_dummy_data(
-#             name = get_random_string(),
-#             price = get_random_price(10,20)
-#     )
-
-
-# def test_size_generator():
-
-#     assert SizeGenerator.create_dummy_data(
-#             name = get_random_string,
-#             price = get_random_price(10,20)
-#     )
-
-# def test_order_genarator():
-
-#     assert OrderGenerator.create_dummy_data(
-#             customer = CustomerGenerator.create_dummy_data(
-#             customer_address = get_random_string(),
-#             customer_dni = get_random_sequence(),
-#             customer_name = get_random_string(),
-#             customer_phone = get_random_sequence()            
-#         ),
-#         ingredients = IngredientGenerator. create_dummy_data(
-#             name = get_random_string(),
-#             price = get_random_price(10,20)
-#         ),
-#         size = SizeGenerator.create_dummy_data(
-#             name = get_random_string(),
-#             price = get_random_price(10,20)           
-#         ), 
-#     )
+    assert create_data.main() == {}
