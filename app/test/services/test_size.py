02d91e71fd73dfb54e7755edf9ad8acab476d2b5
Nicole Alvarez <nicole.alvarez@ioet.com>
2022-09-19 23:22:10 -0500

    fix(orders bug): create new endpoint for return sizes

--- app/test/services/test_size.py
+++ app/test/services/test_size.py
@@ -1,10 +1,8 @@
 import pytest
 
-def test_get_size_service(client, create_size, size_uri):
+def test_get_size_service(client, create_sizes, size_uri):
     response = client.get(size_uri)
     pytest.assume(response.status.startswith('200'))
-    returned_sizes = {
-        size['_id']: size for size in response.json}
-    for size in create_size:
+    returned_sizes = {size['_id']: size for size in response.json}
+    for size in create_sizes:
         pytest.assume(size['_id'] in returned_sizes)
-        
