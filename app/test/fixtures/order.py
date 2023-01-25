6052bb5d210f8a65e8d96fa2720aba59e88a1efa
Davoyandun <yandundavid@gmail.com>
2023-01-25 18:55:02 -0500

    feat: add ci

--- app/test/fixtures/order.py
+++ app/test/fixtures/order.py
@@ -24,7 +24,7 @@ def client_data():
 
 
 @pytest.fixture
-def order(create_ingredients, create_size, client_data) -> dict:
+def order(create_ingredients, create_size) -> dict:
     ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
     size_id = create_size.get('_id')
     return {
@@ -35,15 +35,20 @@ def order(create_ingredients, create_size, client_data) -> dict:
 
 
 @pytest.fixture
-def create_orders(client, order_uri, create_ingredients, create_sizes) -> list:
+def create_orders(
+    client, order_uri, create_ingredients, create_sizes
+) -> list:
     ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
     sizes = [size.get('_id') for size in create_sizes]
     orders = []
     for _ in range(10):
-        new_order = client.post(order_uri, json={
-            **client_data_mock(),
-            'ingredients': shuffle_list(ingredients)[:5],
-            'size_id': shuffle_list(sizes)[0]
-        })
+        new_order = client.post(
+            order_uri,
+            json={
+                **client_data_mock(),
+                'ingredients': shuffle_list(ingredients)[:5],
+                'size_id': shuffle_list(sizes)[0],
+            },
+        )
         orders.append(new_order)
     return orders
