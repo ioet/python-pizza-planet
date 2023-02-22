f248a968c859e0d4482408f506e6fc517d818a2e
cordova00 <anthony.cordova@ioet.com>
2023-02-22 01:32:00 -0500

    Refactor the services to make the logic more efficient

--- app/services/beverage.py
+++ app/services/beverage.py
@@ -1,38 +1,31 @@
 from app.common.http_methods import GET, POST, PUT
 from flask import Blueprint, jsonify, request
 
+from app.services.base_service import BaseService
+
 from ..controllers import BeverageController
 
 beverage = Blueprint('beverage', __name__)
 
 
+beverage_base_service = BaseService(entity = "beverage", 
+        entitycontroller = BeverageController())
+
 @beverage.route('/', methods=POST)
 def create_beverage():
-    beverage, error = BeverageController.create(request.json)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+     return beverage_base_service.create(request)
 
 
 @beverage.route('/', methods=PUT)
 def update_beverage():
-    beverage, error = BeverageController.update(request.json)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+     return beverage_base_service.update(request)
 
 
 @beverage.route('/id/<_id>', methods=GET)
 def get_beverage_by_id(_id: int):
-    beverage, error = BeverageController.get_by_id(_id)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if beverage else 404 if not error else 400
-    return jsonify(response), status_code
+     return beverage_base_service.get_by_id(_id)
 
 
 @beverage.route('/', methods=GET)
 def get_beverages():
-    beverages, error = BeverageController.get_all()
-    response = beverages if not error else {'error': error}
-    status_code = 200 if beverages else 404 if not error else 400
-    return jsonify(response), status_code
+     return beverage_base_service.get_all()
--- app/services/size.py
+++ app/services/size.py
@@ -1,37 +1,27 @@
 from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request
+
+from app.services.base_service import BaseService
 
 from ..controllers import SizeController
 
 size = Blueprint('size', __name__)
 
+size_base_service = BaseService(entity = "ingredient", 
+        entitycontroller = SizeController())
 
 @size.route('/', methods=POST)
 def create_size():
-    size, error = SizeController.create(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
-
+    return size_base_service.create(request)
 
 @size.route('/', methods=PUT)
 def update_size():
-    size, error = SizeController.update(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
-
+    return size_base_service.update(request)
 
 @size.route('/id/<_id>', methods=GET)
 def get_size_by_id(_id: int):
-    size, error = SizeController.get_by_id(_id)
-    response = size if not error else {'error': error}
-    status_code = 200 if size else 404 if not error else 400
-    return jsonify(response), status_code
+    return size_base_service.get_by_id(_id)
 
 @size.route('/', methods=GET)
 def get_sizes():
-    sizes, error = SizeController.get_all()
-    response = sizes if not error else {'error': error}
-    status_code = 200 if sizes else 404 if not error else 400
-    return jsonify(response), status_code
+    return size_base_service.get_all()
