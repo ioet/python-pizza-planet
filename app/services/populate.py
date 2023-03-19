a331ce160409ef2886bd82d020be0721ccb541db
Constancio Molinengo <constancio.molinengo@outlook.com>
2023-03-19 01:48:52 -0300

    feat: [PIZ5] create new endpoint to populate db

---
+++ app/services/populate.py
@@ -0,0 +1,9 @@
+from app.fakedata.generate_fake_data import main
+from flask import Blueprint
+
+populate = Blueprint('report', __name__)
+
+@populate.before_app_first_request
+def populate_fake_data():
+    print('Populating fake data...')
+    main()
