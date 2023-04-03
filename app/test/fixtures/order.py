889cefa6f2ae21d244191a9498b58f59e4569863
cordova00 <anthony.cordova@ioet.com>
2023-04-03 08:54:06 -0500

    Refactor the tests to performance the TDD

--- app/test/fixtures/order.py
+++ app/test/fixtures/order.py
@@ -1,23 +1,15 @@
-import json
-
 import pytest
-from datetime import datetime
-from faker import Faker
-from faker.generator import random
-from app.repositories.models import Ingredient, Size, Beverage
+
 from ..utils.functions import (shuffle_list, get_random_sequence,
-                               get_random_string, get_random_phone, get_random_choice, create_random_date,
-                               )
-from ...controllers import IngredientController, SizeController
+                               get_random_string)
 
 
 def client_data_mock() -> dict:
-    fake = Faker()
     return {
-        'client_address': fake.address(),
-        'client_dni': fake.random_number(digits=8),
-        'client_name': fake.name(),
-        'client_phone': fake.phone_number()
+        'client_address': get_random_string(),
+        'client_dni': get_random_sequence(),
+        'client_name': get_random_string(),
+        'client_phone': get_random_sequence()
     }
 
 
@@ -32,46 +24,37 @@ def client_data():
 
 
 @pytest.fixture
-def clients_data():
-    return [client_data_mock() for _ in range(3)]
-
-@pytest.fixture
-def order(create_beverages, create_ingredients, create_size, client_data) -> dict:
+def order(create_ingredients, create_size, client_data) -> dict:
     ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
-    beverages = [beverage.get('_id') for beverage in create_beverages]
-    size_id = create_size.json['_id']
+    size_id = create_size.get('_id')
     return {
-        **client_data,
+        **client_data_mock(),
         'ingredients': ingredients,
-        'beverages': beverages,
         'size_id': size_id
     }
-    
-@pytest.fixture
-def create_order(client, order_uri, create_ingredients, create_beverages, create_size, client_data):
+
+
+def create_order(client, order_uri, create_ingredients, create_sizes):
     ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
-    beverages = [beverage.get('_id') for beverage in create_beverages]
-    size_id = create_size.json['_id']
-    order_data = {
-        **client_data,
-        'ingredients': ingredients,
-        'beverages': beverages,
-        'size_id': size_id,
-        'date': create_random_date()
-    }
-    return order_data
+    sizes = [size.get('_id') for size in create_sizes]
+    order = client.post(order_uri, json={
+        **client_data_mock(),
+        'ingredients': shuffle_list(ingredients)[:5],
+        'size_id': shuffle_list(sizes)[0]
+    })
+    return order
+
 
 @pytest.fixture
-def get_order_by_id():
-    fake = Faker()
-    order = {
-        '_id': fake.uuid4(),
-        'client_name': fake.name(),
-        'client_dni': fake.random_number(digits=8),
-        'client_address': fake.address(),
-        'client_phone': fake.phone_number(),
-        'date': fake.date_time_this_decade(),
-        'total_price': fake.random_number(),
-        'size': {'_id': fake.random_number()}
-    }
-    return order
+def create_order_dict(client, order_uri, create_ingredients, create_sizes) -> dict:
+    return create_order(client, order_uri, create_ingredients, create_sizes)
+
+
+@pytest.fixture
+def create_orders_list(client, order_uri, create_ingredients, create_sizes) -> list:
+    orders = []
+    for _ in range(10):
+        new_order = create_order(
+            client, order_uri, create_ingredients, create_sizes)
+        orders.append(new_order)
+    return orders
